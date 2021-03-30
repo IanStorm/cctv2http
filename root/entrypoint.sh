1cff1f0aee7e7833cc26ab2b33a75133e045596d
Christian Sporer <IanStorm28@outlook.com>
2021-03-30 15:19:00 +0200

    Decrease latency by dropping frames

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -5,9 +5,11 @@ nginx
 ffmpeg \
 	-i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov \
 	-fflags nobuffer \
+	-filter:v fps=2 \
 	-vcodec libx264 -vprofile baseline \
 	-an \
 	-strict -2 \
 	-f flv rtmp://127.0.0.1/push-hls/live
-# 	-fflags nobuffer :: reduce latency
-# 	-an :: remove audio
+# 	-fflags nobuffer => reduce latency
+# 	-filter:v => drop frames
+# 	-an => remove audio
