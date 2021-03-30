85bd69cc0fc2b899fe025f0ee0b7bd64e2b49da4
Christian Sporer <IanStorm28@outlook.com>
2021-03-30 18:04:58 +0200

    Reduce CPU load and latency

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -6,23 +6,23 @@ ffmpeg \
 	-stream_loop -1 \
 	-rtsp_transport tcp \
 	-i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov \
-	-fflags nobuffer \
 	-vcodec libx264 \
 		-movflags +faststart \
-		-preset fast \
+		-preset superfast \
 		-tune zerolatency \
 		-vf \
-			"fps=fps=2, \
-			 scale=1280:720" \
+			"fps=fps=1, \
+			 scale=-1:720" \
+		-b:v 100k \
 	-an \
 	-f flv rtmp://127.0.0.1/push-hls/live
 # 	-stream_loop => infinitely loop input stream
 # 	-rtsp_transport => prefer/enforce TCP (over UDP)
-# 	-fflags nobuffer => reduce latency
 # 	-vcodec => output H.264
 # 		-movflags +faststart => recommended for browser env., see https://trac.ffmpeg.org/wiki/Encode/H.264
 # 		-preset => reduce latency
 # 		-tune => reduce latency
 # 		-vf fps => drop frames
 # 		-vf scale => scale output
+# 		-b:v => limit bit rate
 # 	-an => remove audio
