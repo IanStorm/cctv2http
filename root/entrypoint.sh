711c40ce6bbf8238e1a6064aa702c9e60ea30bcf
Christian Sporer <IanStorm28@outlook.com>
2021-03-30 16:24:14 +0200

    Supplement to f7711281f8db3b7ff28240016ef3d89802775371

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -3,6 +3,8 @@
 nginx
 
 ffmpeg \
+	-stream_loop -1 \
+	-rtsp_transport tcp \
 	-i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov \
 	-fflags nobuffer \
 	-vcodec libx264 \
@@ -14,6 +16,8 @@ ffmpeg \
 			 scale=1280:720" \
 	-an \
 	-f flv rtmp://127.0.0.1/push-hls/live
+# 	-stream_loop => infinitely loop input stream
+# 	-rtsp_transport => prefer/enforce TCP (over UDP)
 # 	-fflags nobuffer => reduce latency
 # 	-vcodec => output H.264
 # 		-movflags +faststart => recommended for browser env., see https://trac.ffmpeg.org/wiki/Encode/H.264
